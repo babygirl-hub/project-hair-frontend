613c6b49340ed29c3061ea2ba864bc8d472205f6
babygirl-hub <anokwuruchinomso@gmail.com>
2025-05-23 12:58:39 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from './components/Hero';
+import ProductList from './components/ProductList';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="bg-gray-100 text-gray-900">
+      <Hero />
+      <ProductList />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
