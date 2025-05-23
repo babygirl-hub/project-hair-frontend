eddce8f737399e297be830d89d93734420c1c266
babygirl-hub <anokwuruchinomso@gmail.com>
2025-05-23 13:26:42 +0100

    Create ProductList.jsx

---
+++ public/ProductList.jsx
@@ -0,0 +1,5 @@
+const products = [
+  { id: 1, name: 'Body Wave Bundle', price: '$120', image: '/bundle1.jpg' },
+  { id: 2, name: 'Straight Lace Wig', price: '$250', image: '/wig1.jpg' },
+  { id: 3, name: 'Curly Closure', price: '$90', image: '/closure1.jpg' },
+];
